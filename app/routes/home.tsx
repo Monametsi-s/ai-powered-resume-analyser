1bbf1e012a8a472e22057f0e5616c480fb28b97e
Monametsi <elboncaptain@gmail.com>
2025-08-22 08:07:28 +0200

    feat: restructure Home component to enhance layout and display resume ratings

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CV Analyser" },
+    { name: "description", content: "Smart resume feedback!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      
+      <section className="main-section">
+      <div className="page-heading">
+        <h1>
+          Track your applications & Resume Ratings
+        </h1>
+        <Navbar />
+
+        <h2>
+          Review submissions and check for AI powered feedback
+        </h2>
+        
+      </div>
+    </section>
+
+    {resumes.length && (
+      <div className="resumes-section">
+         {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume}/>
+      ))}
+      </div>
+    )}
+     
+  </main>
 }
