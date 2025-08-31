7447b585106b4dc787d1bee02d7ffbc6338a36a4
Monametsi <elboncaptain@gmail.com>
2025-08-31 22:11:55 +0200

    chore: clean up component formatting and syntax

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,20 +1,20 @@
-export const meta = () => ([
-    {title: 'CVAnalyser | Auth'},
-    {name: 'description', content: 'Login to your account'},
-])
+export const meta = () => [
+  { title: "CVAnalyser | Auth" },
+  { name: "description", content: "Login to your account" },
+];
 const auth = () => {
   return (
     <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
-        <div className="gradient-border shadow-lg">
-            <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
-                <div className="flex flex-col items-center gap-2 text-center">
-                    <h1>Welcome</h1>
-                    <h2>Log In to Continue Your Job Journey</h2>
-                </div>
-            </section>
-        </div>
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <div className="flex flex-col items-center gap-2 text-center">
+            <h1>Welcome</h1>
+            <h2>Log In to Continue Your Job Journey</h2>
+          </div>
+        </section>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default auth
+export default auth;
